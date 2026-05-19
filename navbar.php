6cee73a7b759fbc24313eeb93840d6e20e59bd58
wbotha1019-crypto <wbotha1019@gmail.com>
2026-05-19 20:11:57 +0200

    New dynamic navbar

---
+++ navbar.php
@@ -0,0 +1,10 @@
+<nav class="navbar">
+  <div class="nav-group">
+    <a href="/COS221-PA5-main/index.html">Home</a>
+    <a href="/COS221-PA5-main/traveller/browsePackages.php">Browse</a>
+  </div>
+  <div class="nav-group">
+    <span class="user-display">Welcome</span>
+    <a href="/COS221-PA5-main/logout.php" class="logout-btn">Logout</a>
+  </div>
+</nav>

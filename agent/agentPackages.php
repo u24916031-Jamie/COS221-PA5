787a16e2cad748c11364aef3597f0ca0ff1f681e
wbotha1019-crypto <wbotha1019@gmail.com>
2026-05-22 14:48:17 +0200

    Actual php pages for edit/del/update

---
+++ agent/agentPackages.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My Packages</title>
+    <link rel="stylesheet" href="../css/navbar.css" />
+    <link rel="stylesheet" href="../css/browsePackages.css" /> 
+</head>
+<body>
+    <?php include '../navbar.php'; ?>
+    <div class="browse-container">
+        <h1 style="color: white; text-shadow: 2px 2px 4px rgba(0,0,0,0.5);">My Offered Packages</h1>
+        <div class="packages-grid" id="packagesGrid"></div>
+    </div>
+    <script src="../js/agentPackages.js"></script>
+</body>
+</html>

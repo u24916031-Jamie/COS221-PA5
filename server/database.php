52594a31027052c5edc96d29e6ba2f8249272fbb
u24916031-Jamie <u24916031@tuks.co.za>
2026-05-24 12:36:32 +0200

    changed database .env.example

--- server/database.php
+++ server/database.php
@@ -14,7 +14,7 @@ class Database {
 	}
 	private function __construct() { 
 		$host = 'localhost'; //change host to the local or sm
-		$env = parse_ini_file('.env.example'); // create a .env file
+		$env = parse_ini_file('.env'); // create a .env file
 		$username = $env['USERNAME'];
 		$password = $env['PASSWORD'];
 		$dbname = $env['DBNAME'];
